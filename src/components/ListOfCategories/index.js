1882e9d8faf6fd7af6a218ba7b1c8f6959abd9ec
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-01 21:03:37 -0500

    fetch categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Category } from "../Category";
 
 import { List, Item } from "./styles";
-import { categories } from "../../../api/db.json";
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const { data } = await axios.get(
+        "https://petgram-server-dagibu.vercel.app/categories"
+      );
+      setCategories(data);
+    };
+
+    fetchCategories();
+  }, []);
+
   return (
     <List>
       {categories.map((category) => (
         <Item key={category.id}>
-          <Category {...category}/>
+          <Category {...category} />
         </Item>
       ))}
     </List>
