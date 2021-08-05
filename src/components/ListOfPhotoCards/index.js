ae7fe391e7479795052bd697938f758607e8ab72
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-04 19:41:58 -0500

    filter photos

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,33 +1,23 @@
 import React from "react";
 
 import { useQuery } from "@apollo/react-hooks";
-import { gql } from "apollo-boost";
 import { PhotoCard } from "../PhotoCard";
+import { GET_PHOTOS } from "../../graphql/photos";
 
-const getPhotos = gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`;
-
-export const ListOfPhotoCards = () => {
-  const { loading, error, data } = useQuery(getPhotos);
+export const ListOfPhotoCards = ({ categoryId }) => {
+  const { loading, error, data } = useQuery(GET_PHOTOS, {
+    variables: { categoryId },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
   return (
     <ul>
-      {data && data.photos.map((photoCard, id) => (
-        <PhotoCard key={id} {...photoCard} />
-      ))}
+      {data &&
+        data.photos.map((photoCard, id) => (
+          <PhotoCard key={id} {...photoCard} />
+        ))}
     </ul>
   );
 };
