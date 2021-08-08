4e3b67cb8fbe4ec7e5531d342968c17ac712eb9f
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-07 19:43:40 -0500

    apollo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import { App } from "./App";
 
 const client = new ApolloClient({
   uri: "https://petgram-server-dagibu.vercel.app/graphql",
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
