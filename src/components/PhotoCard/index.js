8667cb70f3391492e9e5df3be127b23efa588234
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-09 18:05:03 -0500

    mutation

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,33 +1,38 @@
-import React, { Fragment, useEffect, useRef, useState } from "react";
-import { Article, ImgWrapper, Img, Button } from "./styles";
-import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import React from "react";
+import { ImgWrapper, Img, Article } from "./styles";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useNearScreen } from "../../hooks/useNearScreen";
-
+import { useMuationToogleLike } from "../../hooks/useMutationToogleLike";
+import { FavButton } from "../FavButton";
 const DEFAULT_IMAGE =
-  "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+  "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen();
+  const { mutation, mutationLoading, mutationError } = useMuationToogleLike();
   const key = `like-${id}`;
   const [liked, setLiked] = useLocalStorage(key, false);
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+  const handleFavClick = () => {
+    !liked &&
+      mutation({
+        variables: {
+          input: { id },
+        },
+      });
+    setLiked(!liked);
+  };
 
   return (
     <Article ref={element}>
       {show && (
-        <Fragment>
+        <>
           <a href={`/?detail=${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
           </a>
-
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size="32px" /> {likes} likes!
-          </Button>
-        </Fragment>
+          <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+        </>
       )}
     </Article>
   );
