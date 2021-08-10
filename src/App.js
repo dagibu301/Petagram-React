98d6aa717062bfdecb6513b334295631c66b41fe
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-10 09:19:00 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
-import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
+import { NavBar } from './components/NavBar';
 
 import { Router } from "@reach/router";
 
@@ -17,6 +17,7 @@ export const App = () => {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <NavBar />
     </div>
   );
 };
