1060e3460dd564f95017a0f24a3049edfe180943
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-14 18:39:00 -0500

    form

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Context from "../Context";
+import { UserForm } from "../components/UserForm";
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
-        <form onSubmit={activateAuth}>
-          <button>Sign in</button>
-        </form>
+        <Fragment>
+          <UserForm title="Register" onSubmit={activateAuth} />
+          <UserForm title="Sign in" onSubmit={activateAuth} />
+        </Fragment>
       );
     }}
   </Context.Consumer>
