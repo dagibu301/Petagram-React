8c5fe44e93c65dbecd539b9739b612d0c872d241
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-17 19:49:21 -0500

    context

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,10 +1,11 @@
-import React from "react";
-import Context from "../Context";
+import React, { useContext } from "react";
+import { Context } from "../Context";
 import { UserForm } from "../components/UserForm";
 import { useRegisterMutation } from "../hooks/useRegisterMutation";
 import { useLoginMutation } from "../hooks/useLoginMutation";
 
 export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context);
   const {
     registerMutation,
     data: dataReg,
@@ -18,58 +19,49 @@ export const NotRegisteredUser = () => {
     loading: loadingLog,
   } = useLoginMutation();
 
+  const registerSubmit = ({ email, password }) => {
+    const input = { email, password };
+    const variables = { input };
+    registerMutation({ variables })
+      .then(({ data }) => {
+        const { signup } = data;
+        activateAuth(signup);
+      })
+      .catch((err) => {
+        console.log("El usuario ya existe o hay algún problema.");
+      });
+  };
+  const errorRegMsg = errorReg && "El usuario ya existe o hay algún problema.";
+
+  const loginSubmit = ({ email, password }) => {
+    const input = { email, password };
+    const variables = { input };
+    loginMutation({ variables })
+      .then(({ data }) => {
+        const { login } = data;
+        activateAuth(login);
+      })
+      .catch((err) => {
+        console.log("La contraseña no es correcta o el usuario no existe.");
+      });
+  };
+  const errorLogMsg =
+    errorLog && "La contraseña no es correcta o el usuario no existe.";
+
   return (
-    <Context.Consumer>
-      {({ activateAuth }) => {
-        const registerSubmit = ({ email, password }) => {
-          const input = { email, password };
-          const variables = { input };
-          registerMutation({ variables })
-            .then(({ data }) => {
-              const { signup } = data;
-              activateAuth(signup);
-            })
-            .catch((err) => {
-              console.log("El usuario ya existe o hay algún problema.");
-            });
-        };
-        const errorRegMsg =
-          errorReg && "El usuario ya existe o hay algún problema.";
-
-        const loginSubmit = ({ email, password }) => {
-          const input = { email, password };
-          const variables = { input };
-          loginMutation({ variables })
-            .then(({ data }) => {
-              const { login } = data;
-              activateAuth(login);
-            })
-            .catch((err) => {
-              console.log(
-                "La contraseña no es correcta o el usuario no existe."
-              );
-            });
-        };
-        const errorLogMsg =
-          errorLog && "La contraseña no es correcta o el usuario no existe.";
-
-        return (
-          <>
-            <UserForm
-              disabled={loadingReg}
-              error={errorRegMsg}
-              title="Registrarse"
-              onSubmit={registerSubmit}
-            />
-            <UserForm
-              disabled={loadingLog}
-              error={errorLogMsg}
-              title="Iniciar sesión"
-              onSubmit={loginSubmit}
-            />
-          </>
-        );
-      }}
-    </Context.Consumer>
+    <>
+      <UserForm
+        disabled={loadingReg}
+        error={errorRegMsg}
+        title="Reister"
+        onSubmit={registerSubmit}
+      />
+      <UserForm
+        disabled={loadingLog}
+        error={errorLogMsg}
+        title="Sign in"
+        onSubmit={loginSubmit}
+      />
+    </>
   );
 };
