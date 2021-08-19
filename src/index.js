2281e8086fae63b3881fe6a7a7d648cd88dfcc5a
David Giraldo <davidgiraldobueno@gmail.com>
2021-08-18 20:06:49 -0500

    user like

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import Context from "./Context";
 
 import { App } from "./App";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "https://petgram-server-dagibu.vercel.app/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = window.sessionStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
